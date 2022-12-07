88b278dc3218d8443a109ac95bf14e8b09d9d939
barterjke <113022172+barterjke@users.noreply.github.com>
2022-12-07 12:34:38 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-sudo apt update 
-sudo apt-get install python3.8 python3.8-dev build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev -y
-sudo rm /usr/bin/python3
-sudo ln -s python3.8 /usr/bin/python3
-sudo python3 -m pip install -U pip
+apt update 
+apt-get install python3.8 python3.8-dev build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev -y
+rm /usr/bin/python3
+ln -s python3.8 /usr/bin/python3
+python3 -m pip install -U pip
 pip3 install -r req.txt
